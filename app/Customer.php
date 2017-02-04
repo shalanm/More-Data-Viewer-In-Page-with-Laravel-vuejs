97639df9d1c8f65481f3ba46d8b4646561e9d7e1
shalanm <shalanmohamed222@yahoo.com>
2017-02-04 14:31:42 +0200

    More-Data-Viewer-In-Page-with-Laravel-vuejs

---
+++ app/Customer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Helper\DataViewer;
+
+class Customer extends Model
+{
+    use DataViewer;
+
+    public static $columns = [
+
+      'id' , 'name' , 'email' , 'phone' , 'created_at' , 'updated_at' 
+
+
+
+    ];
+}
---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Customer;
+
+class CustomerController extends Controller
+{
+    public function index(){
+        return view('customer.index');
+    }
+    
+    public function getData(){
+    	$model =  Customer::SearchPaginateAndOrder();
+    	$columns =  Customer::$columns;
+
+    	return response()
+    	->json([
+    		'model' =>$model, 
+    		'columns' =>$columns
+
+    		]);
+        
+    }
+    
+}
